c95b2dc2c14b7fb26a6c4ade1ea14aaad0cfa52f
Abrar Shariar <shariarabrar@gmail.com>
2019-03-24 23:51:01 +0900

    new code

--- summarizer.py
+++ summarizer.py
@@ -1,71 +1,31 @@
-# import PyPDF2
-# # file path to research paper goes here
-# file_path = './papers/p20-legout.pdf'
-# pdfFileObj = open(file_path,'rb')
-# pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-# print(pdfReader.numPages)
-# # get to a particular page
-# pageObj = pdfReader.getPage(2)
-# text_data = pageObj.extractText();
-#
-# print(text_data)
-
-import io
-
-from pdfminer.converter import TextConverter
-from pdfminer.pdfinterp import PDFPageInterpreter
-from pdfminer.pdfinterp import PDFResourceManager
-from pdfminer.pdfpage import PDFPage
-
-import pdfminer
-import sys
-
-# print("Sys path: ", sys.path)
-# exit()
-
-# data mining shit goes here
-def extract_text_from_pdf(pdf_path):
-    resource_manager = PDFResourceManager()
-    fake_file_handle = io.StringIO()
-    converter = TextConverter(resource_manager, fake_file_handle)
-    page_interpreter = PDFPageInterpreter(resource_manager, converter)
-
-    laparams = pdfminer.layout.LAParams()
-
-    # setattr(laparams, "all_texts", True)
-    # setattr(laparams, "detect_vertical", True)
-    # setattr(laparams, "word_margin", 1.0)
-    # setattr(laparams, "char_margin", 1.0)
-    # setattr(laparams, "line_margin", 1.0)
-    # setattr(laparams, "boxes_flow", 1.0)
+import PyPDF2
+import textract
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 
-    for param in ("all_texts", "detect_vertical", "word_margin", "char_margin", "line_margin", "boxes_flow"):
-        paramv = locals().get(param, None)
-        print("param: ", param)
-        print("paramv: ", paramv)
-        if paramv is not None:
-            setattr(laparams, param, paramv)
-        else:
-            laparams = None
+#write a for-loop to open many files -- leave a comment if you'd #like to learn how
+filename = './papers/p20-legout';
+#open allows you to read the file
+pdfFileObj = open(filename,'rb')
+#The pdfReader variable is a readable object that will be parsed
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+#discerning the number of pages will allow us to parse through all #the pages
+num_pages = pdfReader.numPages
+count = 0
+text = ""
+#The while loop will read each page
+while count < num_pages:
+    pageObj = pdfReader.getPage(count)
+    count +=1
+    text += pageObj.extractText()
+#This if statement exists to check if the above library returned #words. It's done because PyPDF2 cannot read scanned files.
+if text != "":
+   text = text
+#If the above returns as False, we run the OCR library textract to #convert scanned/image based PDF files into text
+else:
+   text = textract.process(fileurl, method='tesseract', language='eng')
+# Now we have a text variable which contains all the text derived #from our PDF file. Type print(text) to see what it contains. It #likely contains a lot of spaces, possibly junk such as '\n' etc.
+# Now, we will clean our text variable, and return it as a list of keywords.
 
-    # exit()
-
-
-    with open(pdf_path, 'rb') as fh:
-        for page in PDFPage.get_pages(fh,
-                                      caching=True,
-                                      check_extractable=True):
-            page_interpreter.process_page(page)
-
-        text = fake_file_handle.getvalue()
-
-    # close open handles
-    converter.close()
-    fake_file_handle.close()
-
-    if text:
-        return text
-
-if __name__ == '__main__':
-    print(extract_text_from_pdf('./papers/p20-legout.pdf'))
+print(text)
---
+++ summarizer_old.py
@@ -0,0 +1,60 @@
+# import PyPDF2
+# # file path to research paper goes here
+# file_path = './papers/p20-legout.pdf'
+# pdfFileObj = open(file_path,'rb')
+# pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+# print(pdfReader.numPages)
+# # get to a particular page
+# pageObj = pdfReader.getPage(2)
+# text_data = pageObj.extractText();
+#
+# print(text_data)
+
+import io
+
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfpage import PDFPage
+
+import pdfminer
+import sys
+
+# print("Sys path: ", sys.path)
+# exit()
+
+# data mining shit goes here
+def extract_text_from_pdf(pdf_path):
+    resource_manager = PDFResourceManager()
+    fake_file_handle = io.StringIO()
+    converter = TextConverter(resource_manager, fake_file_handle)
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+
+    laparams = pdfminer.layout.LAParams()
+
+    # setattr(laparams, "all_texts", True)
+    # setattr(laparams, "detect_vertical", True)
+    # setattr(laparams, "word_margin", 1.0)
+    # setattr(laparams, "char_margin", 1.0)
+    # setattr(laparams, "line_margin", 1.0)
+    # setattr(laparams, "boxes_flow", 1.0)
+    # exit()
+
+
+    with open(pdf_path, 'rb') as fh:
+        for page in PDFPage.get_pages(fh,
+                                      caching=True,
+                                      check_extractable=True):
+            page_interpreter.process_page(page)
+
+        text = fake_file_handle.getvalue()
+
+    # close open handles
+    converter.close()
+    fake_file_handle.close()
+
+    if text:
+        return text
+
+if __name__ == '__main__':
+    print(extract_text_from_pdf('./papers/p20-legout.pdf'))
